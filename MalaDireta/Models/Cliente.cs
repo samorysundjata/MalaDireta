43fdda6bb766c48473aee4e062da0d85a8fb581e
Samory Sundjata <samory.sundjata@gmail.com>
2023-04-17 13:54:06 -0300

    Models

---
+++ MalaDireta/Models/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace MalaDireta.Models
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+
+        public string Telefone { get; set; }
+
+        public string Email { get; set; }
+    }
+}
