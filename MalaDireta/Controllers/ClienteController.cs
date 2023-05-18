4200445ed20ce6440d39d21b30e2246bea298241
Samory Sundjata <samory.sundjata@gmail.com>
2023-05-18 16:39:59 -0300

    Iniciando a edição dos controllers.

--- MalaDireta/Controllers/ClienteController.cs
+++ MalaDireta/Controllers/ClienteController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MalaDireta.Context;
+using MalaDireta.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MalaDireta.Controllers
 {
+    [Route("[controller]")]
+    [ApiController]
     public class ClienteController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+
+        public ClienteController(AppDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        [HttpGet("clientes")]
+        public ActionResult<IEnumerable<Cliente>> GetClientes() 
+        {
+            try
+            {
+                var clientes = _context.Clientes.ToList();
+                if (clientes.Any()) { return NotFound("Escrever"); }
+                return Ok(clientes);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                                               "Ocorreu um problema ao tratar a sua solicitação.");
+            }
         }
     }
 }
--- MalaDireta/Controllers/EnderecoController.cs
+++ MalaDireta/Controllers/EnderecoController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MalaDireta.Context;
+using MalaDireta.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MalaDireta.Controllers
 {
+    [Route("[controller]")]
+    [ApiController]
     public class EnderecoController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+
+        public EnderecoController(AppDbContext context)
         {
-            return View();
+            _context = context;
         }
+
+        [HttpGet("enderecos")]
+        public ActionResult<IEnumerable<Endereco>> GetEnderecos() 
+        {
+            try
+            {
+                var enderecos = _context.Clientes.ToList();
+                if (!enderecos.Any()) { return NotFound("Endereços não encontrados"); }
+                //Verificar se o retorno precisa ser Json mesmo.
+                return Json(enderecos);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                                               "Ocorreu um problema ao tratar a sua solicitação.");
+            }
+        }
+
+
     }
 }
