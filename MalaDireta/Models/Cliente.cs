f04292bd38c3cd6fe17f558b8dcd32d5e8254cb2
Samory Sundjata <samory.sundjata@gmail.com>
2023-06-15 14:38:08 -0300

    ClienteTests

--- MalaDireta/Models/Cliente.cs
+++ MalaDireta/Models/Cliente.cs
@@ -6,6 +6,25 @@ namespace MalaDireta.Models
     [Table("Clientes")]
     public class Cliente
     {
+        //Criar validações aqui.
+
+        private int id;
+        private string nome;
+        private string telefone;
+        private string email;
+
+        public Cliente()
+        {
+        }
+
+        public Cliente(int _id, string _nome, string _telefone, string _email)
+        {
+            this.id = _id;
+            this.nome = _nome;
+            this.telefone = _telefone;
+            this.email = _email;
+        }
+
         [Key] //Mudar no futuro para ClienteId
         public int Id { get; set; }
 
