b01dfec291b3d09d2dcc7a10b470a02997fe6c43
Samory Sundjata <samory.sundjata@gmail.com>
2023-06-28 08:28:17 -0300

    Início implementação extensions

---
+++ MalaDireta/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace MalaDireta.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseExceptionHandling(this IApplicationBuilder app, IWebHostEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            return app;
+        }
+
+        //public static IApplicationBuilder UseAppCors(this IApplicationBuilder app) 
+
+        //public static IApplicationBuilder UseSwaggerMiddleware(this IApplicationBuilder app)
+    }
+}
---
+++ MalaDireta/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace MalaDireta.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        //public static WebApplicationBuilder AddApiSwagger(this WebApplicationBuilder builder) 
+
+        //public static IServiceCollection AddSwagger(this IServiceCollection services)
+
+        //public static WebApplicationBuilder AddPersistence(this WebApplicationBuilder builder) 
+
+        //public static WebApplicationBuilder AddAutenticationJwt(this WebApplicationBuilder builder)
+    }
+}
