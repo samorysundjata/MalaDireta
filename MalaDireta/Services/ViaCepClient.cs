ca70449e638b174fcda6e4030b7fa461bb5a765b
Samory Sundjata <samory.sundjata@gmail.com>
2023-08-31 08:54:24 -0300

    ViaCep arquivos

---
+++ MalaDireta/Services/ViaCepClient.cs
@@ -0,0 +1,35 @@
+﻿namespace MalaDireta.Services
+{
+    public class ViaCepClient : IViaCepClient
+    {
+        private const string _baseUrl = "https://viacep.com.br";
+
+        private readonly HttpClient _httpClient;
+
+        public ViaCepClient()
+        {
+            _httpClient = new HttpClientFactory.Create();
+
+        }
+
+        public ViaCepResult Search(string zipCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ViaCepResult> Search(string stateInitials, string city, string address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ViaCepResult> SearchAsync(string zipCode, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ViaCepResult>> SearchAsync(string stateInitials, string city, string address, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
