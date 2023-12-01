992542c6789bf0896ea5df6b2fd06975b3342979
Samory Sundjata <samory.sundjata@gmail.com>
2023-12-01 14:33:41 -0300

    Início dos testes.

---
+++ MalaDiretaTests/Controllers/EnderecoControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using MalaDireta.Controllers;
+using MalaDireta.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq.AutoMock;
+using System.ComponentModel;
+using Xunit;
+
+namespace MalaDiretaTests.Controllers
+{
+    public class EnderecoControllerTests
+    {
+        readonly AutoMocker _mocker;
+        readonly EnderecoController _controller;
+
+        public EnderecoControllerTests()
+        {
+            _mocker = new AutoMocker();
+            _controller = _mocker.CreateInstance<EnderecoController>();
+        }
+
+        [Fact(DisplayName = "Falhar Get")]
+        [Trait("Category", "Fail")]
+        public ActionResult FalharGet()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+    }
+}
